6d68186446ecf70ca5c15a01f1e736225014cd4a
amaganzini <andy.zini@gmail.com>
2024-12-04 19:04:18 -0800

    day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -10,6 +10,8 @@ def parse_args():
 
     parser.add_argument('filename',
                         help='input filename')
+    parser.add_argument('-2', '--part2', action='store_true', default=False,
+                        help='perform part 2 of the puzzle!')
 
     args = parser.parse_args()
 
@@ -30,6 +32,39 @@ def read_muls(filename):
     return muls
 
 
+def read_do_muls(filename):
+    """Returns all the valid ENABLED mul()'s in a file"""
+
+    r_do = re.compile(r'do\(\)')
+    r_dont = re.compile(r'don\'t\(\)')
+    r_mul = re.compile(r'mul\((\d+),(\d+)\)')
+
+    enabled_segments = []
+    muls = []
+
+    buf = open(filename, 'r').read()
+
+    #split on every occurance of don't
+    dont_splits = r_dont.split(buf)
+
+    #start is enabled
+    enabled_segments += [dont_splits.pop(0)]
+
+    #split the don't splits by do
+    for i, segment in enumerate(dont_splits):
+
+        do_split = r_do.split(segment)
+
+        #after splitting the splits that start with don't() on do(), every split after the first is valid
+        enabled_segments += do_split[1::]
+
+    #for segment in enabled_segments:
+    for segment in enabled_segments:
+        muls += r_mul.findall(segment)
+
+    return muls
+
+
 def process_muls(muls):
     """returns the processed sum of a list of mul instructions"""
 
@@ -44,7 +79,10 @@ def process_muls(muls):
 def cli_main(args):
     """main entry"""
 
-    muls = read_muls(args.filename)
+    if args.part2:
+        muls = read_do_muls(args.filename)
+    else:
+        muls = read_muls(args.filename)
 
     mul_val = process_muls(muls)
 
