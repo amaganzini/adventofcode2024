688d76e107634fe970880c6dde6d87dd0f8bb2b8
amaganzini <andy.zini@gmail.com>
2024-12-04 23:03:07 -0800

    day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -10,6 +10,8 @@ def parse_args():
 
     parser.add_argument('filename',
                         help='input filename')
+    parser.add_argument('-2', '--part2', action='store_true', default=False,
+                        help='perform part 2 of the puzzle!')
 
     args = parser.parse_args()
 
@@ -113,15 +115,74 @@ def word_search_count(word, field):
     return count
 
 
+def check_for_cross_word_at(x, y, word, field):
+    """Checks a specific position for the presence of a cross word"""
+
+    start = word[0]
+    center = word[1]
+    end = word[2]
+
+    if field[y][x] != center:
+        return False
+
+    #only have to check 2 adjacent "corners" of the cross
+    '''
+    M . S
+    . A .
+    M . S
+    '''
+
+    #upper left corner word
+    x_range = range(x - 1, x + 2)
+    y_range = range(y - 1, y + 2)
+    upper_left = ''.join([field[y][x] for x, y in zip(x_range, y_range)])
+
+    if upper_left != word and upper_left[::-1] != word:
+        return False
+
+    #upper right corner word
+    x_range = range(x + 1, x - 2, -1)
+    y_range = range(y - 1, y + 2)
+    upper_right = ''.join([field[y][x] for x, y in zip(x_range, y_range)])
+
+    if upper_right != word and upper_right[::-1] != word:
+        return False
+
+    print('Found a cross of %s at %d, %d' % (word, x, y))
+    return True
+    
+
+def cross_word_search_count(word, field):
+    """Returns the number of times the target cross word appears in a cross word search"""
+
+    if(len(word) != 3):
+        raise Exception('This only works for words of length 3, like \'MAS\' :)')
+
+    length = len(field[0]) - 1
+    height = len(field) - 1
+    count = 0
+
+    #We can check locations for the center of the cross
+    #So we can trim off the first and last col and row
+    for y in range(1, height):
+        for x in range(1, length):
+            count += 1 if check_for_cross_word_at(x, y, word, field) else 0
+
+    return count
+
+
 def cli_main(args):
     """main entry"""
 
     TARGET_WORD = "XMAS"
+    TARGET_CROSS = "MAS"
 
     word_field = read_input_into_field(args.filename)
 
-
-    word_count = word_search_count(TARGET_WORD, word_field)
+    if args.part2:
+        word_count = cross_word_search_count(TARGET_CROSS, word_field)
+    else:
+        word_count = word_search_count(TARGET_WORD, word_field)
 
     print('Found %s %d times!' % (TARGET_WORD, word_count))
 
